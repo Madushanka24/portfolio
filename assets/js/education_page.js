7b345fedb8c10ba2fc9c54351a59112dd82056d4
Madushanka24 <madushanka20021224@gmail.com>
2024-06-14 15:57:50 +0530

    Assignments js

--- assets/js/education_page.js
+++ assets/js/education_page.js
@@ -21,3 +21,21 @@ function detectDivSizeChanges() {
     var divWidth = parseInt(assignmentDivComputedStyle.getPropertyValue('width')); /*this get's the width of the div by getPropertyValue and convert it to int then store it in a variable*/
     var divHeight = parseInt(assignmentDivComputedStyle.getPropertyValue('height')); /*this get's the height of the div by getPropertyValue and convert it to int then store it in a variable*/
 
+    if (divWidth === 155 && divHeight === 180) {
+        // Applying changes to the background size in the checkbox event listener
+        var checkboxesAssignments = document.querySelectorAll('.card-02');
+        var containersAssignments = document.querySelectorAll('.assignment-card');
+
+        checkboxesAssignments.forEach(function(checkbox, index) {
+            checkbox.addEventListener('change', function() {
+                // If checkbox is checked
+                if (this.checked) {
+                    containersAssignments[index].style.borderColor = '#eab520';
+                    containersAssignments[index].style.backgroundSize = '110px 110px';
+                } else {
+                    containersAssignments[index].style.borderColor = '#192A56';
+                    containersAssignments[index].style.backgroundSize = '175px 160px';
+                }
+            });
+        });
+    }
