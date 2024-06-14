a0f484ad882a57d14c46a48f99d175dd47230c67
Madushanka24 <madushanka20021224@gmail.com>
2024-06-14 15:56:20 +0530

    Assignments js

--- assets/js/education_page.js
+++ assets/js/education_page.js
@@ -1,3 +1,16 @@
 // Get all the checkboxes and their respective containers in assignments
 var checkboxesAssignments = document.querySelectorAll('.card-02');
 var containersAssignments = document.querySelectorAll('.assignment-card');
+
+checkboxesAssignments.forEach(function(checkbox, index) {
+    checkbox.addEventListener('change', function() {
+        // If checkbox is checked
+        if (this.checked) {
+            containersAssignments[index].style.borderColor = '#eab520';
+            containersAssignments[index].style.backgroundSize = '150px 150px';
+        } else {
+            containersAssignments[index].style.borderColor = '#192A56';
+            containersAssignments[index].style.backgroundSize = '225px 210px';
+        }
+    });
+});
