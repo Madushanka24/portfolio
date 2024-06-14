c64907002adef5170cf81bf8e83ceb9eb223b877
Madushanka24 <madushanka20021224@gmail.com>
2024-06-14 15:55:45 +0530

    Assignments js

---
+++ assets/js/education_page.js
@@ -0,0 +1,3 @@
+// Get all the checkboxes and their respective containers in assignments
+var checkboxesAssignments = document.querySelectorAll('.card-02');
+var containersAssignments = document.querySelectorAll('.assignment-card');
