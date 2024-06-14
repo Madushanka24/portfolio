b6c36169e5a8b0fd68c2904435ebbf361be6e18c
Madushanka24 <madushanka20021224@gmail.com>
2024-06-14 15:57:25 +0530

    Assignments js

--- assets/js/education_page.js
+++ assets/js/education_page.js
@@ -14,3 +14,10 @@ checkboxesAssignments.forEach(function(checkbox, index) {
         }
     });
 });
+
+function detectDivSizeChanges() {
+    var assignmentDiv = document.querySelector('#assignments-cards>div');
+    var assignmentDivComputedStyle = window.getComputedStyle(assignmentDiv);
+    var divWidth = parseInt(assignmentDivComputedStyle.getPropertyValue('width')); /*this get's the width of the div by getPropertyValue and convert it to int then store it in a variable*/
+    var divHeight = parseInt(assignmentDivComputedStyle.getPropertyValue('height')); /*this get's the height of the div by getPropertyValue and convert it to int then store it in a variable*/
+
